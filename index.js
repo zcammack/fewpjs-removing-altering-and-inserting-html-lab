002d88cb2b1fbe492902d19cda6efec1ee35853d
zcammack <zack.cammack@gmail.com>
2021-02-05 16:15:40 -0600

    commit tree 6591bf612e5e333603d1a6d9fb99b3c8374522a7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Zack is the champion';
